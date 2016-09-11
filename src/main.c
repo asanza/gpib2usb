8c1f46b200d27263354f0796f6725914b96e5ce1
Diego Asanza <f.asanza@gmail.com>
2016-09-11 12:10:02 +0200

    beautify code

--- src/main.c
+++ src/main.c
@@ -39,9 +39,9 @@ static uint8_t cdc_interfaces[] = { 0 };
 int temp = 0;
 static void send_string_sync(uint8_t endpoint, const char *str)
 {
-	char *in_buf = (char*) usb_get_in_buffer(endpoint);
+	char *in_buf = (char*)usb_get_in_buffer(endpoint);
 
-	while (usb_in_endpoint_busy(endpoint));
+	while (usb_in_endpoint_busy(endpoint)) ;
 
 	strcpy(in_buf, str);
 	/* Hack: Get the length from strlen(). This is inefficient, but it's
@@ -49,14 +49,17 @@ static void send_string_sync(uint8_t endpoint, const char *str)
 	usb_send_in_buffer(endpoint, strlen(in_buf));
 }
 
-static void write_buffer_sync(const char* str, int len){
-   			char *in_buf = (char*) usb_get_in_buffer(2);
-			while (usb_in_endpoint_busy(2));
-			strncpy(in_buf, str, len);
-			usb_send_in_buffer(2, len);
+static void write_buffer_sync(const char* str, int len)
+{
+	char *in_buf = (char*)usb_get_in_buffer(2);
+
+	while (usb_in_endpoint_busy(2)) ;
+	strncpy(in_buf, str, len);
+	usb_send_in_buffer(2, len);
 }
 
-static void write_char_sync(const char c){
+static void write_char_sync(const char c)
+{
 	write_buffer_sync(&c, 1);
 }
 
@@ -65,34 +68,34 @@ int main(void)
 	char *str;
 	const unsigned char *out_buf;
 	size_t len;
-    hal_sys_init();
+
+	hal_sys_init();
 #ifdef MULTI_CLASS_DEVICE
 	cdc_set_interface_list(cdc_interfaces, sizeof(cdc_interfaces));
 #endif
 	usb_init();
-    hal_sys_green_led_on();
+	hal_sys_green_led_on();
 	while (1) {
-		if(!usb_is_configured()) continue;
-		if(usb_out_endpoint_halted(2)) continue;
+		if (!usb_is_configured()) continue;
+		if (usb_out_endpoint_halted(2)) continue;
 		/* check if data available from gpib bus */
-		if( sys_tasks() ){
+		if ( sys_tasks() ) {
 			/* send data */
-            len = sys_read_gpib(&str);
+			len = sys_read_gpib(&str);
 			write_buffer_sync(str, len);
 			continue;
 		}
-		if(!usb_out_endpoint_has_data(2)) {
-            continue;
-        }
+		if (!usb_out_endpoint_has_data(2))
+			continue;
 		len = usb_get_out_buffer(2, &out_buf);
-        if(len <= 0) usb_arm_out_endpoint(2);
-		if(read_line(out_buf, len) > 0){
+		if (len <= 0) usb_arm_out_endpoint(2);
+		if (read_line(out_buf, len) > 0) {
 			/* complete line received. process. */
 			devcmd cmd;
 			/* process input and get the output buffer */
 			len = process_input(&str);
 			/* send response to usb */
-            write_buffer_sync(str, len);
+			write_buffer_sync(str, len);
 		}
 		usb_arm_out_endpoint(2);
 	}
@@ -169,46 +172,46 @@ int8_t app_send_encapsulated_command(uint8_t interface, uint16_t length)
 }
 
 int16_t app_get_encapsulated_response(uint8_t interface,
-                                      uint16_t length, const void **report,
-                                      usb_ep0_data_stage_callback *callback,
-                                      void **context)
+				      uint16_t length, const void **report,
+				      usb_ep0_data_stage_callback *callback,
+				      void **context)
 {
 	return -1;
 }
 
 void app_set_comm_feature_callback(uint8_t interface,
-                                     bool idle_setting,
-                                     bool data_multiplexed_state)
+				   bool idle_setting,
+				   bool data_multiplexed_state)
 {
 }
 
 void app_clear_comm_feature_callback(uint8_t interface,
-                                       bool idle_setting,
-                                       bool data_multiplexed_state)
+				     bool idle_setting,
+				     bool data_multiplexed_state)
 {
 }
 
 int8_t app_get_comm_feature_callback(uint8_t interface,
-                                     bool *idle_setting,
-                                     bool *data_multiplexed_state)
+				     bool *idle_setting,
+				     bool *data_multiplexed_state)
 {
 	return -1;
 }
 
 void app_set_line_coding_callback(uint8_t interface,
-                                    const struct cdc_line_coding *coding)
+				  const struct cdc_line_coding *coding)
 {
 }
 
 int8_t app_get_line_coding_callback(uint8_t interface,
-                                    struct cdc_line_coding *coding)
+				    struct cdc_line_coding *coding)
 {
 	/* This is where baud rate, data, stop, and parity bits are set. */
 	return -1;
 }
 
 int8_t app_set_control_line_state_callback(uint8_t interface,
-                                           bool dtr, bool dts)
+					   bool dtr, bool dts)
 {
 	return 0;
 }
