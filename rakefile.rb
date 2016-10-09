024b258b5183ae5d9b3eda0a8341a487f5d43a2d
Diego Asanza <f.asanza@gmail.com>
2016-10-09 17:21:19 +0200

    add command line programming task
    Signed-off-by: Diego Asanza <f.asanza@gmail.com>

--- rakefile.rb
+++ rakefile.rb
@@ -4,3 +4,11 @@ load "#{PROJECT_CEEDLING_ROOT}/lib/ceedling.rb"
 Ceedling.load_project
 
 task :default => %w[ test:all release ]
+
+desc "Program gpib2usb adapter using pickit3."
+task :program => :release do
+  hex_file = "build/release/#{RELEASE_BUILD_OUTPUT}.hex"
+  cmd = "ipecmd.sh /P#{ENV['MCU']}  -TPPK3 /E /L /OL -F#{hex_file} -M -Y"
+  puts cmd
+  sh cmd
+end
